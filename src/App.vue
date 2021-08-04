d1f3a19f4e459f9db0cede2b48ee88bfdb2f4457
SonLe <80952802+LeVanSonTMT@users.noreply.github.com>
2021-08-04 17:31:26 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,8 @@ export default {
 }
 </script>
 
+<style src="../public/AdminLTE-3.0.5/dist/css/adminlte.min.css"></style>
+
 <style src="../plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css"></style>
 <style src="../plugins/icheck-bootstrap/icheck-bootstrap.min.css"></style>
 <style src="../plugins/overlayScrollbars/css/OverlayScrollbars.min.css"></style>
